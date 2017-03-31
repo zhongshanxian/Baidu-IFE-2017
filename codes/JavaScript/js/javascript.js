85f3cca664080f9935d83eda03f1f35692f22aee
zhongshanxian <971296739@qq.com>
2017-03-31 12:50:21 +0800

    add task 3

--- codes/JavaScript/js/javascript.js
+++ codes/JavaScript/js/javascript.js
@@ -43,3 +43,39 @@ for(var i=0;i<aqiData.length;i++)
   }
 }
 
+
+//task 3
+function paixu3()
+{
+var source=document.getElementById("source");
+var resort=source;
+var resort1=document.getElementById("resort1");//放置排序结果
+var bList=document.getElementsByTagName("b");
+
+  
+for(var i=1;i<source.childNodes.length-1;i+=2)//提取<b>中的数字进行冒泡排序
+  {
+    for(var j=i+2;j<resort.childNodes.length;j+=2)
+      {  
+        if(resort.childNodes[i].childNodes[1].innerHTML<resort.childNodes[j].childNodes[1].innerHTML)
+          {
+            var temp=resort.childNodes[j].innerHTML;
+            resort.childNodes[j].innerHTML=resort.childNodes[i].innerHTML;
+            resort.childNodes[i].innerHTML=temp;
+          }
+      }
+  }
+for(var i=1;i<resort.childNodes.length;i+=2)
+  {
+        var li=document.createElement("li");//创建一个li元素节点
+        li.innerHTML="第"+(i+1)/2+"名"+" "+resort.childNodes[i].innerHTML;//添加文本
+        resort1.appendChild(li);//显示在resort列表中
+  }
+}
+
+  
+
+
+  
+
+
