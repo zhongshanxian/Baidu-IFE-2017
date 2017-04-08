376664e9bf299554a80a3addab927d3481285825
zhongshanxian <971296739@qq.com>
2017-04-08 19:11:20 +0800

    add task7

---
+++ codes/JavaScript/task7/javascript/task7.js
@@ -0,0 +1,273 @@
+function bianlione1(){
+  var one=document.getElementById("one");
+  one.style.backgroundColor="#d45d5c"; 
+  one.style.transform="scale(1.02)";
+  }
+  function bianlione2(){
+  var one=document.getElementById("one");
+  one.style.backgroundColor="white"; 
+  one.style.transform="scale(1)";
+  }
+  function bianlitwo11(){
+  var two11=document.getElementById("two1");
+  two11.style.backgroundColor="#d45d5c"; 
+  two11.style.transform="scale(1.02)";
+  }
+  function bianlitwo12(){
+  var two11=document.getElementById("two1");
+  two11.style.backgroundColor="white";
+  two11.style.transform="scale(1)";
+  }
+  function bianlithree11(){
+  var three11=document.getElementById("three1");
+  three11.style.backgroundColor="#d45d5c"; 
+  three11.style.transform="scale(1.02)";
+  }
+  function bianlithree12(){
+  var three11=document.getElementById("three1");
+  three11.style.backgroundColor="white";
+  three11.style.transform="scale(1)";
+  }
+  function bianlifour11(){
+  var four11=document.getElementById("four1");
+  four11.style.backgroundColor="#d45d5c"; 
+  four11.style.transform="scale(1.02)";
+  }
+  function bianlifour12(){
+  var four11=document.getElementById("four1");
+  four11.style.backgroundColor="white";
+  four11.style.transform="scale(1)";
+  }
+  function bianlifour21(){
+  var four21=document.getElementById("four2");
+  four21.style.backgroundColor="#d45d5c"; 
+  four21.style.transform="scale(1.02)";
+  }
+  function bianlifour22(){
+  var four21=document.getElementById("four2");
+  four21.style.backgroundColor="white";
+  four21.style.transform="scale(1)";
+  }
+
+  //第二个
+
+  function bianlithree21(){
+  var three21=document.getElementById("three2");
+  three21.style.backgroundColor="#d45d5c"; 
+  three21.style.transform="scale(1.02)";
+  }
+  function bianlithree22(){
+  var three21=document.getElementById("three2");
+  three21.style.backgroundColor="white";
+  three21.style.transform="scale(1)";
+  }
+  function bianlifour31(){
+  var four31=document.getElementById("four3");
+  four31.style.backgroundColor="#d45d5c"; 
+  four31.style.transform="scale(1.02)";
+  }
+  function bianlifour32(){
+  var four31=document.getElementById("four3");
+  four31.style.backgroundColor="white";
+  four31.style.transform="scale(1)";
+  }
+  function bianlifour41(){
+  var four41=document.getElementById("four4");
+  four41.style.backgroundColor="#d45d5c"; 
+  four41.style.transform="scale(1.02)";
+  }
+  function bianlifour42(){
+  var four41=document.getElementById("four4");
+  four41.style.backgroundColor="white";
+  four41.style.transform="scale(1)";
+  }
+
+
+  //右边
+  function bianlitwo21(){
+  var two21=document.getElementById("two2");
+  two21.style.backgroundColor="#d45d5c"; 
+  two21.style.transform="scale(1.02)";
+  }
+  function bianlitwo22(){
+  var two21=document.getElementById("two2");
+  two21.style.backgroundColor="white";
+  two21.style.transform="scale(1)";
+  }
+  function bianlithree31(){
+  var three31=document.getElementById("three3");
+  three31.style.backgroundColor="#d45d5c"; 
+  three31.style.transform="scale(1.02)";
+  }
+  function bianlithree32(){
+  var three31=document.getElementById("three3");
+  three31.style.backgroundColor="white";
+  three31.style.transform="scale(1)";
+  }
+  function bianlifour51(){
+  var four51=document.getElementById("four5");
+  four51.style.backgroundColor="#d45d5c"; 
+  four51.style.transform="scale(1.02)";
+  }
+  function bianlifour52(){
+  var four51=document.getElementById("four5");
+  four51.style.backgroundColor="white";
+  four51.style.transform="scale(1)";
+  }
+  function bianlifour61(){
+  var four61=document.getElementById("four6");
+  four61.style.backgroundColor="#d45d5c"; 
+  four61.style.transform="scale(1.02)";
+  }
+  function bianlifour62(){
+  var four61=document.getElementById("four6");
+  four61.style.backgroundColor="white";
+  four61.style.transform="scale(1)";
+  }
+
+  //第二个
+
+  function bianlithree41(){
+  var three41=document.getElementById("three4");
+  three41.style.backgroundColor="#d45d5c"; 
+  three41.style.transform="scale(1.02)";
+  }
+  function bianlithree42(){
+  var three71=document.getElementById("three4");
+  three71.style.backgroundColor="white";
+  three71.style.transform="scale(1)";
+  }
+  function bianlifour71(){
+  var four71=document.getElementById("four7");
+  four71.style.backgroundColor="#d45d5c"; 
+  four71.style.transform="scale(1.02)";
+  }
+  function bianlifour72(){
+  var four71=document.getElementById("four7");
+  four71.style.backgroundColor="white";
+  four71.style.transform="scale(1)";
+  }
+  function bianlifour81(){
+  var four81=document.getElementById("four8");
+  four81.style.backgroundColor="#d45d5c"; 
+  four81.style.transform="scale(1.02)";
+  }
+  function bianlifour82(){
+  var four81=document.getElementById("four8");
+  four81.style.backgroundColor="white";
+  four81.style.transform="scale(1)";
+  }
+
+//前序
+function qianxu(){
+	//最外层
+  var bianli11=setTimeout(bianlione1,1000);
+  var bianli12=setTimeout(bianlione2,1500);
+  //左边
+  var bianli211=setTimeout(bianlitwo11,2000);
+  var bianli212=setTimeout(bianlitwo12,2500);
+  var bianli311=setTimeout(bianlithree11,3000);
+  var bianli312=setTimeout(bianlithree12,3500);
+  var bianli411=setTimeout(bianlifour11,4000);
+  var bianli412=setTimeout(bianlifour12,4500);
+  var bianli421=setTimeout(bianlifour21,5000);
+  var bianli422=setTimeout(bianlifour22,5500);
+  var bianli321=setTimeout(bianlithree21,6000);
+  var bianli322=setTimeout(bianlithree22,6500);
+  var bianli431=setTimeout(bianlifour31,7000);
+  var bianli432=setTimeout(bianlifour32,7500);
+  var bianli441=setTimeout(bianlifour41,8000);
+  var bianli442=setTimeout(bianlifour42,8500);
+
+  //右边
+  var bianli211=setTimeout(bianlitwo21,9000);
+  var bianli212=setTimeout(bianlitwo22,9500);
+  var bianli311=setTimeout(bianlithree31,10000);
+  var bianli312=setTimeout(bianlithree32,10500);
+  var bianli411=setTimeout(bianlifour51,11000);
+  var bianli412=setTimeout(bianlifour52,11500);
+  var bianli421=setTimeout(bianlifour61,12000);
+  var bianli422=setTimeout(bianlifour62,12500);
+  var bianli321=setTimeout(bianlithree41,13000);
+  var bianli322=setTimeout(bianlithree42,13500);
+  var bianli431=setTimeout(bianlifour71,14000);
+  var bianli432=setTimeout(bianlifour72,14500);
+  var bianli441=setTimeout(bianlifour81,15000);
+  var bianli442=setTimeout(bianlifour82,15500);
+}
+
+function zhongxu(){
+	//左边
+  var bianli211=setTimeout(bianlitwo11,1000);
+  var bianli212=setTimeout(bianlitwo12,1500);
+  var bianli311=setTimeout(bianlithree11,2000);
+  var bianli312=setTimeout(bianlithree12,2500);
+  var bianli411=setTimeout(bianlifour11,3000);
+  var bianli412=setTimeout(bianlifour12,3500);
+  var bianli421=setTimeout(bianlifour21,4000);
+  var bianli422=setTimeout(bianlifour22,4500);
+  var bianli321=setTimeout(bianlithree21,5000);
+  var bianli322=setTimeout(bianlithree22,5500);
+  var bianli431=setTimeout(bianlifour31,6000);
+  var bianli432=setTimeout(bianlifour32,6500);
+  var bianli441=setTimeout(bianlifour41,7000);
+  var bianli442=setTimeout(bianlifour42,7500);
+  //最外层
+  var bianli11=setTimeout(bianlione1,8000);
+  var bianli12=setTimeout(bianlione2,8500);
+
+  //右边
+  var bianli211=setTimeout(bianlitwo21,9000);
+  var bianli212=setTimeout(bianlitwo22,9500);
+  var bianli311=setTimeout(bianlithree31,10000);
+  var bianli312=setTimeout(bianlithree32,10500);
+  var bianli411=setTimeout(bianlifour51,11000);
+  var bianli412=setTimeout(bianlifour52,11500);
+  var bianli421=setTimeout(bianlifour61,12000);
+  var bianli422=setTimeout(bianlifour62,12500);
+  var bianli321=setTimeout(bianlithree41,13000);
+  var bianli322=setTimeout(bianlithree42,13500);
+  var bianli431=setTimeout(bianlifour71,14000);
+  var bianli432=setTimeout(bianlifour72,14500);
+  var bianli441=setTimeout(bianlifour81,15000);
+  var bianli442=setTimeout(bianlifour82,15500);
+}
+
+
+function houxu(){
+	//左边
+  var bianli211=setTimeout(bianlitwo11,1000);
+  var bianli212=setTimeout(bianlitwo12,1500);
+  var bianli311=setTimeout(bianlithree11,2000);
+  var bianli312=setTimeout(bianlithree12,2500);
+  var bianli411=setTimeout(bianlifour11,3000);
+  var bianli412=setTimeout(bianlifour12,3500);
+  var bianli421=setTimeout(bianlifour21,4000);
+  var bianli422=setTimeout(bianlifour22,4500);
+  var bianli321=setTimeout(bianlithree21,5000);
+  var bianli322=setTimeout(bianlithree22,5500);
+  var bianli431=setTimeout(bianlifour31,6000);
+  var bianli432=setTimeout(bianlifour32,6500);
+  var bianli441=setTimeout(bianlifour41,7000);
+  var bianli442=setTimeout(bianlifour42,7500);
+
+  //右边
+  var bianli211=setTimeout(bianlitwo21,8000);
+  var bianli212=setTimeout(bianlitwo22,8500);
+  var bianli311=setTimeout(bianlithree31,9000);
+  var bianli312=setTimeout(bianlithree32,9500);
+  var bianli411=setTimeout(bianlifour51,10000);
+  var bianli412=setTimeout(bianlifour52,10500);
+  var bianli421=setTimeout(bianlifour61,11000);
+  var bianli422=setTimeout(bianlifour62,11500);
+  var bianli321=setTimeout(bianlithree41,12000);
+  var bianli322=setTimeout(bianlithree42,12500);
+  var bianli431=setTimeout(bianlifour71,13000);
+  var bianli432=setTimeout(bianlifour72,13500);
+  var bianli441=setTimeout(bianlifour81,14000);
+  var bianli442=setTimeout(bianlifour82,14500);
+  //最外层
+  var bianli11=setTimeout(bianlione1,15000);
+  var bianli12=setTimeout(bianlione2,15500);
+}
+
