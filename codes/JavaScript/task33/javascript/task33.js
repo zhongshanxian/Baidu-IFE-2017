7a6a7ec73caff10e908cac17ceda5465a1fe627f
zhongshanxian <971296739@qq.com>
2017-04-23 10:00:48 +0800

    add task33

---
+++ codes/JavaScript/task33/javascript/task33.js
@@ -0,0 +1,8 @@
+student.onclick=function(){
+	radio1.style.display="block";
+	radio2.style.display="none";
+}
+worker.onclick=function(){
+	radio2.style.display="block";
+	radio1.style.display="none";
+}
